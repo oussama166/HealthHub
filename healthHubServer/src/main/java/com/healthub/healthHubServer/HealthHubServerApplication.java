6c65224ae1ddcac3f7440950b52612f890f09c2b
oussama166 <oussamaouardi80@gmail.com>
2024-05-02 16:21:57 +0100

    add healthHub server side

---
+++ healthHubServer/src/main/java/com/healthub/healthHubServer/HealthHubServerApplication.java
@@ -0,0 +1,13 @@
+package com.healthub.healthHubServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthHubServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealthHubServerApplication.class, args);
+	}
+
+}
