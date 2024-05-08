509a1adfbe81ca34d9f66462c48dc97e27cb982e
ywatch <94308024+ywatch@users.noreply.github.com>
2024-05-08 15:55:33 +0100

    v1

---
+++ healthHubServer/src/main/java/com/healthub/healthHubServer/Config/SecurityConfig.java
@@ -0,0 +1,7 @@
+package com.healthub.healthHubServer.Config;
+
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@EnableWebSecurity
+public class SecurityConfig {
+}
