ae929162eb9cc842d9155339411a49c7cf44946e
oussama166 <oussamaouardi80@gmail.com>
2024-05-26 18:08:48 +0100

    Refactor the helthhub front end direct

---
+++ healthHubFrontEnd/src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+import {Button} from "@/components/ui/button.tsx";
+
+function App() {
+
+    return (
+        <>
+            <h1 className={"text-center text-3xl"}>Hello Click this button</h1>
+            <Button>
+                Click
+            </Button>
+        </>
+    )
+}
+
+export default App
